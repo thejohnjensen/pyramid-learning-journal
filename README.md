176644f8ca29659f613e5af1feb03d6ce09eed13
jensjoh01 <johnnyj2439@gmail.com>
2017-10-31 17:55:48 -0700

    added requrements.txt, website is working, one page css not working

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pyramid-learning-journal
 
 Personal learning journal using bootstrap css and some basic bootstrap blog html.
+
+**Uses the following routes:**
+`    config.add_route('home', '/')`
+`    config.add_route('detail_view', '/journal/{id:\d+}')`
+`    config.add_route('new_entry', '/journal/new_entry')`
+`    config.add_route('update', '/journal/{id:\d+}/update')`
---
+++ learning_journal/learning_journal/routes.py
@@ -0,0 +1,10 @@
+"""Routes for pages to be served on the web."""
+
+
+def includeme(config):
+    """Include the following routes for static files and uri paths."""
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('detail_view', '/journal/{id:\d+}')
+    config.add_route('new_entry', '/journal/new_entry')
+    config.add_route('update', '/journal/{id:\d+}/update')
