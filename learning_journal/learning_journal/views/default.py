ee33a744d4727466d336050aa0ddcf8d6e7eaaa0
jensjoh01 <johnnyj2439@gmail.com>
2017-11-02 19:48:32 -0700

    working on tests

--- learning_journal/learning_journal/views/default.py
+++ learning_journal/learning_journal/views/default.py
@@ -1,18 +1,20 @@
 from pyramid.view import view_config
-from datetime import datetime
 from pyramid.httpexceptions import HTTPNotFound
 from learning_journal.data.data import journal_dict
 
 
-@view_config(route_name='home', renderer="learning_journal:templates/index.jinja2")
+@view_config(route_name='home',
+             renderer="learning_journal:templates/index.jinja2")
 def list_entry(request):
+    """Function that renders the view of the home page."""
     return {
-    "journals": journal_dict
+        "journals": journal_dict
     }
 
 
 @view_config(route_name='detail_view', renderer="learning_journal:templates/details.jinja2")
 def detail_view(request):
+    """Render the detail view of one journal entry."""
     journal_id = int(request.matchdict['id'])
     if journal_id < 0 or journal_id > len(journal_dict):
         raise HTTPNotFound
