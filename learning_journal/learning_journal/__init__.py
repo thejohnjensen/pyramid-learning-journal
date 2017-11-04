5f3f1ac154ef7eb2a4f4593bcf88423f4535d45a
jensjoh01 <johnnyj2439@gmail.com>
2017-11-04 13:57:27 -0700

    corrected type in url sqlalchemy

--- learning_journal/learning_journal/__init__.py
+++ learning_journal/learning_journal/__init__.py
@@ -5,8 +5,7 @@ import os
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    if os.environ.get('DATABASE_URL', ''):
-        settings["sqlalchem.url"] = os.environ["DATABASE_URL"]
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
